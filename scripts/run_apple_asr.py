12b9ab7fe2f3ce82e0a4956dfe9957737fe898bb
b1n0 <azim4fs@gmail.com>
2025-10-28 23:27:48 +0000

    remove prints

--- scripts/run_apple_asr.py
+++ scripts/run_apple_asr.py
@@ -1,7 +1,6 @@
 from thestage_speechkit.apple import ASRPipeline
 
 pipe = ASRPipeline('TheStageAI/thewhisper-large-v3-turbo', chunk_length_s=10, model_size='S')
-print(pipe.feature_extractor)
 generate_kwargs={
     'max_new_tokens': 128,
     'num_beams': 1,
@@ -9,7 +8,6 @@ generate_kwargs={
     'use_cache': True,
     'language': 'en',
 }
-print(pipe.model.config)
 output = pipe(
     'example_speech.wav', 
     chunk_length_s=10, 
