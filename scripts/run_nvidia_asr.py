c24e76493917f653b66dc83f80eaa94321787b21
b1n0 <azim4fs@gmail.com>
2025-10-28 15:44:17 +0000

    update

--- scripts/run_nvidia_asr.py
+++ scripts/run_nvidia_asr.py
@@ -1,7 +1,7 @@
 from thestage_asr.nvidia import ASRPipeline
 
 pipe = ASRPipeline(
-    'openai/whisper-large-v3-turbo', chunk_length_s=10
+    'openai/whisper-large-v3-turbo', chunk_length_s=10, model_size='S'
 )
-output = pipe('sample_1.mp3', chunk_length_s=10)
+output = pipe('example_speech.wav', chunk_length_s=10)
 print(output)
