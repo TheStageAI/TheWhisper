24a5c18fea270ec21498aac1ced00de590bc0803
b1n0 <azim4fs@gmail.com>
2025-10-28 19:44:45 +0000

    change model

--- scripts/run_nvidia_asr.py
+++ scripts/run_nvidia_asr.py
@@ -8,7 +8,7 @@ generate_kwargs={
 }
 chunk_length_s = 10
 pipe = ASRPipeline(
-    'openai/whisper-large-v3-turbo', 
+    'TheStageAI/thewhisper-large-v3-turbo', 
     chunk_length_s=chunk_length_s, model_size='S', device='cuda'
 )
 output = pipe('example_speech.wav', generate_kwargs=generate_kwargs)
