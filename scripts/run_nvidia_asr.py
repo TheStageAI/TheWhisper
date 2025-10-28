d2b303170170817e200e2f9d792543c53a528152
b1n0 <azim4fs@gmail.com>
2025-10-28 16:03:21 +0000

    fix

--- scripts/run_nvidia_asr.py
+++ scripts/run_nvidia_asr.py
@@ -1,7 +1,13 @@
 from thestage_asr.nvidia import ASRPipeline
 
+generate_kwargs={
+    'num_beams': 1,
+    'do_sample': False,
+    'use_cache': True,
+    'language': 'en',
+}
 pipe = ASRPipeline(
     'openai/whisper-large-v3-turbo', chunk_length_s=10, model_size='S'
 )
-output = pipe('example_speech.wav', chunk_length_s=10)
+output = pipe('example_speech.wav', chunk_length_s=10, generate_kwargs=generate_kwargs)
 print(output)
