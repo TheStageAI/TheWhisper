9e5f2d6d558e8b5a353004c5a0acdf8ad36392e3
Azim Kurbanov <Azim4fs@gmail.com>
2025-10-29 22:06:50 +0300

    small fixes (#27)

--- examples/run_apple_asr.py
+++ examples/run_apple_asr.py
@@ -1,19 +1,19 @@
 from librosa import load, resample
 from thestage_speechkit.apple import ASRPipeline
 
-pipe = ASRPipeline('TheStageAI/thewhisper-large-v3-turbo', chunk_length_s=10, model_size='S')
+chunk_length_s = 10
+pipe = ASRPipeline('TheStageAI/thewhisper-large-v3-turbo', chunk_length_s=chunk_length_s, model_size='S')
 generate_kwargs={
     'max_new_tokens': 128,
     'num_beams': 1,
     'do_sample': False,
     'use_cache': True,
-    'language': 'en',
 }
 audio, sr = load('example_speech.wav')
 audio = resample(audio, orig_sr=sr, target_sr=16000)
 output = pipe(
     audio,
-    chunk_length_s=10, 
+    chunk_length_s=chunk_length_s,
     generate_kwargs=generate_kwargs, 
     return_timestamps='word'
 )
