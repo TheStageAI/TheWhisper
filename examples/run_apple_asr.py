4aaa26a94b0c12a99076433a596033e700a7fa0d
Azim Kurbanov <Azim4fs@gmail.com>
2025-11-24 08:59:19 +0300

    Update chunk length parameter in ASR examples for improved performance (#43)

--- examples/run_apple_asr.py
+++ examples/run_apple_asr.py
@@ -1,7 +1,7 @@
 from librosa import load, resample
 from thestage_speechkit.apple import ASRPipeline
 
-chunk_length_s = 10
+chunk_length_s = 15
 pipe = ASRPipeline('TheStageAI/thewhisper-large-v3-turbo', chunk_length_s=chunk_length_s, model_size='S')
 generate_kwargs={
     'max_new_tokens': 128,
@@ -13,7 +13,7 @@ audio, sr = load('example_speech.wav')
 audio = resample(audio, orig_sr=sr, target_sr=16000)
 output = pipe(
     audio,
-    chunk_length_s=chunk_length_s,
+    chunk_length_s=chunk_length_s-1,
     generate_kwargs=generate_kwargs, 
     return_timestamps='word'
 )
